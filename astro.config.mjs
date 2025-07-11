cd29353db567770ac10c2c6b11aa627258ce231d
Duong Duy Nhat Minh <duongnhatminhbrvt@gmail.com>
2025-07-11 18:22:17 +0700

    Fixed missing CSS error.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), sitemap()]
-  },
-  site: 'https://www.oriviet.org'
+    plugins: [tailwindcss()]
+  }
 });
